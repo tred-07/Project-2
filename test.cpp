4d7da0e099b2746fe6635c8c229f42ed027475dc
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-08-21 00:15:01 +0600

    New

--- test.cpp
+++ test.cpp
@@ -13,20 +13,12 @@
 #define debug(a)
 #define sep(a)
 #endif
-const int N=1e7;
+const int N=1020,m=10;
+bool ok[N];
+int par[N];
 //using namespace __gnu_pbds;
 using namespace std;
 //template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-bitset<N>isprime;
-vector<int>prime;
-void sieve(){
-    for(int i=3;i<=N;i+=2)isprime[i]=1;
-    for(int i=3;i*i <N;i+=2){
-        if(isprime[i])for(int j=i*i;j<N;j+=i)isprime[j]=0;
-    }
-    prime.pb(2);
-    for(int j=3;j<N;j+=2)if(isprime[j])prime.pb(j);
-}
 int power(int x,int y)
 {
     int res=1;
@@ -42,6 +34,21 @@ int power(int x,int y)
     }
     return res;
 }
+int valid(int u,int v){
+    cout<<"?"<<blk<<u<<blk<<v<<nl;
+}
+void dfs(int u,int v){
+    int x=valid(u,v);
+    if(x==u || x==v){
+        par[v]=u;
+        ok[v]=1;
+        return;
+    }
+    if(!ok[x]){
+        dfs(u,x);
+    }
+    dfs(x,v);
+}
 void solve();
 int32_t main()
 {
@@ -51,7 +58,7 @@ int32_t main()
 #endif
 auto cl=clock();
  int ts=1;
-//  cin>>ts;
+ cin>>ts;
  rep(Testcase_,1,ts+1)
  {
     //debug(Testcase_)
@@ -62,5 +69,19 @@ debug(Time)
 }
 void solve()
 {
- sieve();
+ int n;
+ cin>>n;
+ for(int i=1;i<=n;i++)ok[i]=0;
+ ok[1]=1;
+ for(int i=2;i<=n;i++){
+    if(!ok[i])dfs(1,i);
+ }
+ cout<<"!";
+ for(int i=2;i<=n;i++)cout<<i<<blk<<par[i]<<blk;
+ cout<<nl;
 }
+/*
+1
+-1
+3 1 2
+*/
